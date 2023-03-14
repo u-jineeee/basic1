30ebbcdf7dca7e7f7bb21422c0d7531557f2112d
ujineeee <yujin11006@naver.com>
2023-03-14 13:37:25 +0900

    /home/addPerson, /home/people

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,16 +1,23 @@
 package com.ll.basic1;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class HomeController {
     private int cnt;
-
+    private int n;
+    private ArrayList<Person> person;
     HomeController(){
         cnt = -1;
+        n = 0;
+        person = new ArrayList<>();
     }
 
     @GetMapping("/home/main")
@@ -42,4 +49,25 @@ public class HomeController {
     public int showPlus(@RequestParam(defaultValue = "0") int x, @RequestParam int y) {
         return x + y;
     }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String showAddPerson(@RequestParam String name, @RequestParam int age) {
+        person.add(new Person(++n, name, age));
+        return n + "번 사람이 추가되었습니다.";
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public ArrayList<Person> showPeople() {
+        return person;
+    }
+}
+
+@AllArgsConstructor
+@Getter
+class Person {
+    private int id;
+    private String name;
+    private int age;
 }
