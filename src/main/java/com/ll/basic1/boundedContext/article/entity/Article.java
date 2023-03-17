4000217ae36c138e3ea3b41e653e2aa11053f44b
ujineeee <yujin11006@naver.com>
2023-03-17 11:41:14 +0900

    add service

--- src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -1,12 +1,25 @@
 package com.ll.basic1.boundedContext.article.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Builder
+@Getter
 public class Article {
-    @Id
+    @Id //primary key
+    @GeneratedValue(strategy = IDENTITY) //AUTO_INCREMENT
     private long id;
+    private LocalDateTime createDate; // 데이터 생성 날짜
+    private LocalDateTime modifyDate; // 데이터 수정 날짜
     private String title;
     private String body;
+
 }
